fbb1366f1f96068866ded759e655b0495263595a
Enes Algul <86858992+Phenomex@users.noreply.github.com>
2021-12-30 04:55:54 +0000

    Ilk Jscript build oluyor.

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.getElementById("convert-a").getAttribute.length
+var Tyear=document.getElementById(Tyear);
+var Lyear=document.getElementById(Lyear);
+var Tyg=document.getElementById(Tyg);
+var adr=document.getElementById(adr);
+var Dleam=document.getElementById(Dleam);
+var Calculate=document.getElementById(hesapla)
